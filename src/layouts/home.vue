b56c2e9e60755f0fad5a42779a3d493e6935dae1
Etienne Roulet <32195170+etiiiR@users.noreply.github.com>
2022-10-02 11:39:53 +0200

    added

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,9 +1,18 @@
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <RouterView />
+    <Suspense>
+      <!-- component with nested async dependencies -->
+      <RouterView />
+
+      <!-- loading state via #fallback slot -->
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
     <Footer />
     <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
       [Home Layout]
     </div>
   </main>
 </template>
+
