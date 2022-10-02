86797384c74847d695f095492a89bd992c99f448
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2022-10-02 15:02:00 +0200

    For Gabruel

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -104,13 +104,13 @@ const predict = () => {
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block dark:text-white uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-            Mortgage value
+            Mortgage value [in thousands]
           </label>
           <input id="grid-first-name" v-model="mortgage" class=" appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="number" placeholder="Value">
         </div>
         <div class="w-full md:w-1/2 px-3">
           <label class="block dark:text-white uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-            Your income
+            Income [in thousands]
           </label>
           <input id="grid-last-name" v-model="income" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="number" placeholder="Value">
         </div>
@@ -124,7 +124,7 @@ const predict = () => {
         </div>
         <div class="w-full md:w-1/2 px-3">
           <label class="block dark:text-white uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-assets">
-            Your Assets
+            Assets [in thousands]
           </label>
           <input id="grid-assets" v-model="assets" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="number" placeholder="Value">
         </div>
